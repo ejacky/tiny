53d40e57877e61fa81444f2d114fd4d7aa468408
kooBH <k01032633280@gmail.com>
2019-05-03 08:44:30 +0900

    prj2 v1

---
+++ project2_20130523/globals.h
@@ -0,0 +1,138 @@
+/****************************************************/
+/* File: globals.h                                  */
+/* Yacc/Bison Version                               */
+/* Global types and vars for TINY compiler          */
+/* must come before other include files             */
+/* Compiler Construction: Principles and Practice   */
+/* Kenneth C. Louden                                */
+/****************************************************/
+
+#ifndef _GLOBALS_H_
+#define _GLOBALS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/* Yacc/Bison generates internally its own values
+ * for the tokens. Other files can access these values
+ * by including the tab.h file generated using the
+ * Yacc/Bison option -d ("generate header")
+ *
+ * The YYPARSER flag prevents inclusion of the tab.h
+ * into the Yacc/Bison output itself
+ */
+
+#ifndef YYPARSER
+
+/* the name of the following file may change */
+#include "hw2.tab.h"
+
+/* ENDFILE is implicitly defined by Yacc/Bison,
+ * and not included in the tab.h file
+ */
+#define ENDFILE 0
+
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+/* MAXRESERVED = the number of reserved words */
+#define MAXRESERVED 6
+
+/* Yacc/Bison generates its own integer values
+ * for tokens
+ */
+typedef int TokenType;
+
+extern FILE* source; /* source code text file */
+extern FILE* listing; /* listing output text file */
+extern FILE* code; /* code text file for TM simulator */
+
+extern int lineno; /* source line number for listing */
+
+/**************************************************/
+/***********   Syntax tree for parsing ************/
+/**************************************************/
+
+/* 노드 Kind  설정*/
+// TODO
+typedef enum {StmtK,ExpK,DeclK,ParamK,TypeK} NodeKind;
+typedef enum {CompK,IfK,IterK,RetK,ElseK} StmtKind;
+typedef enum {AssignK,OpK,ConstK,IdK,ArrIdK,CallK} ExpKind;
+typedef enum {FuncK,VarK,ArrVarK} DeclKind;
+typedef enum {ArrParamK,NonArrParamK} ParamKind;
+typedef enum {TypeNameK} TypeKind;
+
+/* ArrayAttr is used for attributes for array variables */
+// TODO
+typedef struct arrayAttr {
+    char * name;
+    int size;
+} ArrayAttr;
+
+
+#define MAXCHILDREN 3
+
+/* TODO */
+typedef struct treeNode
+   { struct treeNode * child[MAXCHILDREN];
+     struct treeNode * sibling;
+     int lineno;
+     NodeKind nodekind;
+     union { StmtKind stmt;
+             ExpKind exp;
+             DeclKind decl;
+             ParamKind param;
+             TypeKind type; } kind;
+     union { TokenType op;
+             TokenType type;
+             int val;
+             char * name;
+             ArrayAttr arr;
+             } attr;
+     int type;
+   } TreeNode;
+
+/**************************************************/
+/***********   Flags for tracing       ************/
+/**************************************************/
+
+/* EchoSource = TRUE causes the source program to
+ * be echoed to the listing file with line numbers
+ * during parsing
+ */
+extern int EchoSource;
+
+/* TraceScan = TRUE causes token information to be
+ * printed to the listing file as each token is
+ * recognized by the scanner
+ */
+extern int TraceScan;
+
+/* TraceParse = TRUE causes the syntax tree to be
+ * printed to the listing file in linearized form
+ * (using indents for children)
+ */
+extern int TraceParse;
+
+/* TraceAnalyze = TRUE causes symbol table inserts
+ * and lookups to be reported to the listing file
+ */
+extern int TraceAnalyze;
+
+/* TraceCode = TRUE causes comments to be written
+ * to the TM code file as code is generated
+ */
+extern int TraceCode;
+
+/* Error = TRUE prevents further passes if an error occurs */
+extern int Error;
+#endif
